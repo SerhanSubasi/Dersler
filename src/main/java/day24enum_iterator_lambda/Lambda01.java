48e6a981f3468a02e2e722f957c77940fbcba2dc
Serhan Subaşı <subasiserhan@gmail.com>
2023-09-17 23:12:29 +0300

    Batch 189 notları

--- src/main/java/day24enum_iterator_lambda/Lambda01.java
+++ src/main/java/day24enum_iterator_lambda/Lambda01.java
@@ -1,8 +1,20 @@
 package day24enum_iterator_lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda01 {
     public static void main(String[] args) {
+/*
 
+
+1) Lambda "Functional" (Method programmingdir, diğeri "Structurde Programming" (Yapısal) olarak geçiyordu.
+2) Functional programmingte ne yapılacak üzerine yoğunlaşılır.
+    "Structured Programming"de ise nasıl yapılacak üzerine yoğunlaşılır.
+3) Functional Programming'i Collectionlar ve Arrayler ile kullanırız.
+4) Lambda (Functional Programming) java 8 ile kullanılmaya başlanmıştır.
+                */
 
 
 
