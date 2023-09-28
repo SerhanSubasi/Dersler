60a9375904617205226151f452eef34448d0370b
SerhanSubasi <subasiserhan@gmail.com>
2023-09-28 20:00:48 +0300

    son

--- src/main/java/Practice/Deneme.java
+++ src/main/java/Practice/Deneme.java
@@ -3,7 +3,7 @@ package Practice;
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Merhaba 4" +
+        System.out.println("Merhaba 5" +
                 "");
     }
 }
